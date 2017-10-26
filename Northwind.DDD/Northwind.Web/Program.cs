f0c16790f2ecdf84abe9a66d1a3ed585ff23c2c6
Can PERK <canperk@outlook.com>
2017-10-26 15:08:10 +0300

    infrastrcuture step

--- Northwind.DDD/Northwind.Web/Program.cs
+++ Northwind.DDD/Northwind.Web/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Northwind.DDD.Web
+namespace Northwind.Web
 {
     public class Program
     {
--- Northwind.DDD/Northwind.Web/Startup.cs
+++ Northwind.DDD/Northwind.Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Northwind.DDD.Web
+namespace Northwind.Web
 {
     public class Startup
     {
